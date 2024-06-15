ca86910fbb376b1280f881c6ab3c0283d48f1ad7
karington mitchell <karingtonmitchell3@gmail,com>
2024-06-14 19:24:47 -0600

    sdfghjkl

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -46,5 +46,7 @@ class ListingActivity : Activity{
         Console.Clear();
         Console.WriteLine("Welcome to the Listing Activity.\n");
         Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+        GetInput(0);
+        
     }
 }
